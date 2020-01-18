82536619328060626d558d5e0d8eb3935ac4e3d7
Gabriel Oduori <gabriel.oduori@gmail.com>
2020-01-18 23:57:11 +0300

    Added workspace for tree data structure.

---
+++ L4_Trees/binary_tree.py
@@ -0,0 +1,23 @@
+class Node:
+    def __init__(self):
+        self.value = None
+        self.left = None
+        self.right = None
+
+
+
+node0 = Node()
+
+# node0 = Node("apple")
+# node1 = Node("banana")
+# node2 = Node("orange")
+
+
+# node0.set_left_child(node1)
+# node0.set_right_child(node2)
+
+print(f"""
+value:{node0.value}
+left:{node0.left}
+right:{node0.right}
+""")
