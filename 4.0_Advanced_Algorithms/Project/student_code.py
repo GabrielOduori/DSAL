0c8247d9b618481a7474c5408ce15f42dac5e309
Gabriel Oduori <gabriel.oduori@gmail.com>
2020-03-03 23:48:18 +0300

    Added heuristic method to the module.

--- 4.0_Advanced_Algorithms/Project/student_code.py
+++ 4.0_Advanced_Algorithms/Project/student_code.py
@@ -5,7 +5,25 @@ path - An array of integers which corresponds to a valid sequence of intersectio
 """
 
 
+
+def heuristic(a,b):
+  (x1,y1)  = a
+  (x2,y2)  = b
+
+  return abs(x1-x2) + abs(y1-y2)
+
+
+
 def shortest_path(M,start,goal):
-  
+
     print("shortest path called")
     return
+
+
+
+path = shortest_path(map_40, 5, 34)
+if path == [5, 16, 37, 12, 34]:
+    print("great! Your code works for these inputs!")
+else:
+    print("something is off, your code produced the following:")
+    print(path)
