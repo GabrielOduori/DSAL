4153aa7dd033b212f6d11c9839d8773faa237f63
Gabriel Oduori <gabriel.oduori@gmail.com>
2020-03-20 23:34:38 +0300

    Updated early exit method to break if the start == goal.

--- 4.0_Advanced_Algorithms/Project/student_code.py
+++ 4.0_Advanced_Algorithms/Project/student_code.py
@@ -1,11 +1,11 @@
 # Code inspitaion borroweed from https://www.redblobgames.com/pathfinding/a-star/implementation.html
 
-
-
-
 import math
-#  Using the function-based interface provided by heapq instead of building a class-based interface.
 from queue import PriorityQueue
+
+"""
+Using the function-based interface provided by heapq instead of building a class-based interface.
+"""
 
 
 def shortest_path(M,start,goal):
@@ -13,25 +13,26 @@ def shortest_path(M,start,goal):
     frontier= PriorityQueue()
     frontier.put(start, 0)
     
-    prev = {start: None}
-    score = {start: 0}
+    came_from = {start: None}
+    cost_so_far = {start: 0}
 
     while not frontier.empty():
         current = frontier.get()
 
         if current == goal:
-            generate_path(prev, start, goal)
+            generate_path(came_from, start, goal)
 
         for node in M.roads[current]:
-            updateScore = score[current] + heuristic(M.intersections[current], M.intersections[node])
+            updated_cost = cost_so_far[current] + heuristic(M.intersections[current], M.intersections[node])
             
-            if node not in score or updateScore < score[node]:
-                score[node] = updateScore
-                totalScore = updateScore + heuristic(M.intersections[current], M.intersections[node])
-                frontier.put(node, totalScore)
-                prev[node] = current
+            if node not in cost_so_far or updated_cost < cost_so_far[node]:
+                cost_so_far[node] = updated_cost
+                priority = updated_cost + heuristic(M.intersections[current], M.intersections[node])
+                frontier.put(node, priority)
+                came_from[node] = current
 
-    return generate_path(prev, start, goal)
+    return generate_path(came_from, start, goal)
+
 
 
 #Calculate heuristic
@@ -39,12 +40,13 @@ def heuristic(start, goal):
     return math.sqrt(((start[0] - goal[0]) ** 2) + ((start[1] - goal[1]) ** 2))
 
 
+
 #returning distance from start to goal
-def generate_path(prev, start, goal):
+def generate_path(came_from, start, goal):
     current = goal
     path = [current]
     while current != start:
-        current = prev[current]
+        current = came_from[current]
         path.append(current)
     path.reverse()
     return path
